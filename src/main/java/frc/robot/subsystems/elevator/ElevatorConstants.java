807aa6e8c8735ac77c30f52d2b350c0a9e50e014
Driver Station 120 <gladiatorsprogramming1591@gmail.com>
2025-12-03 19:41:34 -0500

    Start switching from CTRE to REV elevator
    Doesn't yet build

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.elevator;
 
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
 public final class ElevatorConstants {
 
   // CAN IDs
@@ -7,7 +12,7 @@ public final class ElevatorConstants {
   public static final int FOLLOWER_ID = 1; // motor 1 = follower
 
   // Current limit (amps)
-  public static final double SUPPLY_CURRENT_LIMIT = 75.0;
+  public static final int SUPPLY_CURRENT_LIMIT = 75;
 
   // Motion / mechanism constants (tune as needed)
   public static final double GEAR_RATIO = 10.0; // motor rotations per elevator stage rotation
@@ -18,10 +23,16 @@ public final class ElevatorConstants {
   public static final double kP = 30.0;
   public static final double kI = 0.0;
   public static final double kD = 0.5;
+  
+  //Optional values
   public static final double kS = 0.0;
   public static final double kG = 0.5;
   public static final double kV = 3.0;
   public static final double kA = 0.0;
+
+  // FF Values
+  public static final double FF_UP = 0.623;
+  public static final double FF_DOWN = 0.623;
 
   // Position limits (meters)
   public static final double MIN_HEIGHT_METERS = 0.0;
@@ -31,8 +42,62 @@ public final class ElevatorConstants {
   public static final double MAX_VELOCITY_MPS = 1.0;
   public static final double MAX_ACCELERATION_MPS2 = 2.0;
 
+  public static final double INCHES_PER_INTERNAL_ROTATION = 22.0 / 9.0 / 4.0;
+        // public static final double UPPER_SPROCKET_CIRCUMFERENCE_OLD = 1.7576 * Math.PI; // 22 tooth sprocket
+        public static final double UPPER_SPROCKET_CIRCUMFERENCE = 22.0 / 4.0; // 22 tooth sprocket
+        public static final double INCHES_PER_EXTERNAL_ROTATION = UPPER_SPROCKET_CIRCUMFERENCE / 2;
+        public static final double INTERNAL_ROTS_PER_EXTERNAL_ROT = INCHES_PER_EXTERNAL_ROTATION / INCHES_PER_INTERNAL_ROTATION;
+        public static final double INITIAL_HEIGHT_INCHES = 0;
+        public static final double TOLERANCE_INCHES = 1.0;
+
+  public static final double MAX_VEL_DOWN = 3500 / INTERNAL_ROTS_PER_EXTERNAL_ROT;
+        public static final double MAX_ACCEL_DOWN = 5400 / INTERNAL_ROTS_PER_EXTERNAL_ROT * 1.75;
+        public static final double ALLOWERD_ERR_DOWN = 1.0 / INTERNAL_ROTS_PER_EXTERNAL_ROT / 4;
+
+  
+
   // Sim constants
   public static final double SIM_CARRIAGE_MOMENT = 4.0; // arbitrary inertia
+  public static final SparkFlexConfig MOTOR_CONFIG = new SparkFlexConfig() {
+            {
+                idleMode(IdleMode.kBrake);
+                smartCurrentLimit(ElevatorConstants.SUPPLY_CURRENT_LIMIT);
+                inverted(true);
+                // closedLoopRampRate(ElevatorConstants.RAMP_RATE); // Not used; negatively impacts deceleration while approaching hardstops.
+                limitSwitch.reverseLimitSwitchEnabled(false);
+                limitSwitch.forwardLimitSwitchEnabled(false);
 
+                closedLoop.outputRange(-1.0, MAX_VELOCITY_MPS, ClosedLoopSlot.kSlot0) // kslot 0 is up (only kPosition is used)
+                        .p(ElevatorConstants.kP, ClosedLoopSlot.kSlot0)
+                        .i(ElevatorConstants.kI, ClosedLoopSlot.kSlot0)
+                        .d(ElevatorConstants.kD, ClosedLoopSlot.kSlot0)
+                        .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder);
+                // closedLoop.maxMotion.maxVelocity(MAX_VEL_UP, ClosedLoopSlot.kSlot0) // We don't use maxMotion going up due to inconsistent results.
+                //         .maxAcceleration(MAX_ACCEL_UP, ClosedLoopSlot.kSlot0)
+                //         .allowedClosedLoopError(ALLOWERD_ERR_UP, ClosedLoopSlot.kSlot0)
+                //         .positionMode(MAXMotionPositionMode.kMAXMotionTrapezoidal, ClosedLoopSlot.kSlot0);
+
+                closedLoop.outputRange(-1.0, 1, ClosedLoopSlot.kSlot1) // kslot 1 is down (kMAXMotionPositionControl is used)
+                        .p(ElevatorConstants.kP, ClosedLoopSlot.kSlot1)
+                        .i(ElevatorConstants.kI, ClosedLoopSlot.kSlot1)
+                        .d(ElevatorConstants.kD, ClosedLoopSlot.kSlot1)
+                        .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder);
+                closedLoop.maxMotion.maxVelocity(MAX_VEL_DOWN, ClosedLoopSlot.kSlot1)
+                        .maxAcceleration(MAX_ACCEL_DOWN, ClosedLoopSlot.kSlot1)
+                        .allowedClosedLoopError(ALLOWERD_ERR_DOWN, ClosedLoopSlot.kSlot1);
+            }
+        };
+
+        public static final double kSTOW = 0.4;
+        public static final double kL1 = 2.75;
+        public static final double kL2 = 7.04;
+        public static final double kL3 = 15.3;
+        public static final double kL4 = 26.4;
+        public static final double kPROCESSOR = 0.5;
+        public static final double kNET = 27.75;
+        public static final double ALGAE_HIGH = 10.53;
+        public static final double ALGAE_LOW = 2.75;
+        
   private ElevatorConstants() {}
+
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOReal.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOReal.java
@@ -1,51 +1,105 @@
 package frc.robot.subsystems.elevator;
 
+import java.util.EnumMap;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.elevator.ElevatorIO.ElevatorIOInputs;
+
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
 public class ElevatorIOReal implements ElevatorIO {
 
-  private final TalonFX leader = new TalonFX(ElevatorConstants.LEADER_ID);
-  private final TalonFX follower = new TalonFX(ElevatorConstants.FOLLOWER_ID);
+  private final SparkFlex leader = new SparkFlex(ElevatorConstants.LEADER_ID, MotorType.kBrushless);
+  private final SparkFlex follower = new SparkFlex(ElevatorConstants.FOLLOWER_ID, MotorType.kBrushless);
 
+   DigitalInput lowerLimit;
+    Trigger zeroTrigger;
+
+    SparkFlex leaderLeft;
+    SparkFlex followerRight;
+
+    RelativeEncoder externalEncoder; // Through-bore encoder
+    SparkClosedLoopController controller;
+    SparkLimitSwitch bottomLimitSwitch;
+
+    private double lastPos;
+    private boolean printInternalEncZero = true; // Flag to indicate whether to print when we are zeroing during stow position
+    private boolean printBothEncZero = true;
+
+    EnumMap<elevatorPositions, Double> mapEnc = new EnumMap<>(elevatorPositions.class);
+
+    public enum elevatorPositions {
+        STOW,
+        L1,
+        L2,
+        L3,
+        L4,
+        PROCESSOR, // same as stow height?
+        NETSHOOT, // same as l4 height
+        ALGAE_HIGH,
+        ALGAE_LOW, 
+        AUTO_L4
+    }
   public ElevatorIOReal() {
     configureMotors();
   }
 
   private void configureMotors() {
     // Leader configuration
-    TalonFXConfiguration leaderConfig = new TalonFXConfiguration();
-    leaderConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    leaderConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    leader.configure(ElevatorConstants.MOTOR_CONFIG,
+                ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
 
-    leaderConfig.CurrentLimits.SupplyCurrentLimit = ElevatorConstants.SUPPLY_CURRENT_LIMIT;
-    leaderConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        follower.configure(
+                ElevatorConstants.MOTOR_CONFIG.follow(ElevatorConstants.LEADER_ID, true),
+                SparkBase.ResetMode.kResetSafeParameters,
+                SparkBase.PersistMode.kPersistParameters);
 
-    leader.getConfigurator().apply(leaderConfig);
+        externalEncoder = leaderLeft.getExternalEncoder();
+        controller = leaderLeft.getClosedLoopController();
 
-    // Follower configuration
-    TalonFXConfiguration followerConfig = new TalonFXConfiguration();
-    followerConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    // Inverted relative to leader – per your requirement
-    followerConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        bottomLimitSwitch = followerRight.getReverseLimitSwitch();
+        zeroTrigger = new Trigger(this::isElevatorNotAtBottom);
+        zeroTrigger.onFalse(zeroElevatorInternalEncCommand().alongWith(zeroElevatorExternalEncCommand()));
 
-    followerConfig.CurrentLimits.SupplyCurrentLimit = ElevatorConstants.SUPPLY_CURRENT_LIMIT;
-    followerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        lastPos = ElevatorConstants.kSTOW;
 
-    follower.getConfigurator().apply(followerConfig);
-
-    // Follow leader, with opposing direction
-    follower.setControl(new Follower(leader.getDeviceID(), true));
+        mapEnc.put(elevatorPositions.STOW, ElevatorConstants.kSTOW);
+        mapEnc.put(elevatorPositions.L1, ElevatorConstants.kL1);
+        mapEnc.put(elevatorPositions.L2, ElevatorConstants.kL2);
+        mapEnc.put(elevatorPositions.L3, ElevatorConstants.kL3);
+        mapEnc.put(elevatorPositions.L4, ElevatorConstants.kL4);
+        mapEnc.put(elevatorPositions.PROCESSOR, ElevatorConstants.kPROCESSOR);
+        mapEnc.put(elevatorPositions.NETSHOOT, ElevatorConstants.kNET);
+        mapEnc.put(elevatorPositions.ALGAE_HIGH, ElevatorConstants.ALGAE_HIGH);
+        mapEnc.put(elevatorPositions.ALGAE_LOW, ElevatorConstants.ALGAE_LOW);
   }
 
   @Override
   public void updateInputs(ElevatorIOInputs inputs) {
     // Convert motor rotations to meters:
     //   pos_rot * (2πr / gearRatio)
-    double motorRotations = leader.getPosition().getValueAsDouble();
+    double motorRotations = getExternalPositionRotations();
     double motorRPS = leader.getVelocity().getValueAsDouble();
 
     double metersPerRotation =
@@ -69,4 +123,39 @@ public class ElevatorIOReal implements ElevatorIO {
   public void zeroPosition() {
     leader.setPosition(0.0);
   }
+
+  private boolean isElevatorNotAtBottom() {
+        return !bottomLimitSwitch.isPressed();
+    }
+
+  public Command zeroElevatorExternalEncCommand() {
+    return new InstantCommand(() -> {
+        System.out.println("ZeroExternalEncCommand");
+        externalEncoder.setPosition(0);
+    });
+}
+
+public void ElevatorToPositionExternalEnc(elevatorPositions positions) {
+  lastPos = mapEnc.get(positions);
+  setPositionExternalRotations(inchesToExternalRotations(lastPos));
+}
+
+public double inchesToExternalRotations(double inches) {
+  return (inches - ElevatorConstants.INITIAL_HEIGHT_INCHES) / ElevatorConstants.INCHES_PER_EXTERNAL_ROTATION;
+}
+
+public void setPositionExternalRotations(double rotations) {
+  if (rotations < getExternalPositionRotations()) {
+      controller.setReference(rotations, ControlType.kMAXMotionPositionControl, ClosedLoopSlot.kSlot1, ElevatorConstants.FF_DOWN);
+      System.out.println("Lowering Elevator using kSlot1. Reference set to: " + rotations);
+  } else {
+      controller.setReference(rotations, ControlType.kPosition, ClosedLoopSlot.kSlot0, ElevatorConstants.FF_UP);
+      System.out.println("Raising Elevator using kSlot0. Reference set to:  " + rotations);
+  }
+}
+
+public double getExternalPositionRotations() {
+  return externalEncoder.getPosition();
+}
+
 }
